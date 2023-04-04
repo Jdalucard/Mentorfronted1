1f4c35a756e58a87af61f1ee9c0ce377e2e3d37a
Jose Daniel <99700736+Jdalucard@users.noreply.github.com>
2023-04-04 12:53:46 -0500

    "commit1"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CardProduct from './components/CardProduct';
+
+const App = () => {
+  return (
+    <>
+      <CardProduct />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import imagen from "../assets/image-product-desktop.jpg";
+import imagen2 from "../assets/image-product-mobile.jpg";
+import "./styles.css";
+
+const CardProduct = () => {
+  return (
+    <>
+      <main>
+        <div className="product">
+          <picture className="product__img">
+            <img src={imagen} alt="Gabrielle esssmce eau De Parfum<" />
+            <source srcSet={imagen2} media="(min-width:600px)"></source>
+          </picture>
+          <div className="product__content">
+            <p className="product__category"> Perfume</p>
+            <h1 className="product__title">Gabrielle esssmce eau De Parfum</h1>
+            <p>
+              A floral, solar and voluptuos Interpretation composed by oliver
+              Polge, perfumer-creator for the house CHANEL.
+            </p>
+            <div className="flex-Group">
+              <p className="product_price">$ 149.99</p>
+              <p className="product_original_price">
+                <s>$ 169.99</s>
+              </p>
+            </div>
+
+            <button className="button" data-icon="shopping-card" type="submit">
+              Add to Card
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default CardProduct;
